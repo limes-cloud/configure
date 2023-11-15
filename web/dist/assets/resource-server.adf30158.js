1ebfd689f68b8f0c3e966f89d849925e237d39e8
fangweiye <fangweiye@baidu.com>
2023-11-15 22:32:04 +0800

    修改web

---
+++ web/dist/assets/resource-server.adf30158.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index.f90c2f0c.js";function o(r){return e.get("/configure/v1/resource/servers",{params:{resource_id:r}})}function a(r){return e.get("/configure/v1/server/resources",{params:{...r}})}export{o as a,a as p};
